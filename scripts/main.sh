aeff036bc91646cf228d62476a44bd06db42ca38
bart-vanbos_liantis <bart.vanbos@liantis.be>
2025-01-16 17:42:01 +0100

    debug

--- scripts/main.sh
+++ scripts/main.sh
@@ -89,9 +89,6 @@ function execute() {
     local code_scanning_alerts_file="code_scanning_alerts.json"
     local dependabot_alerts_file="dependabot_alerts.json"
     local sbom_file="sbom.json"
-
-    echo ">>>> PWD : $(pwd)"
-    echo ">>>> ENV : $(env)"
     
     # Fetch Code Scanning Alerts from GitHub API
     fetch_code_scanning_alerts \
